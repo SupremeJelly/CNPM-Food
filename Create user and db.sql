8b4929ad122aee373b2c626a5c3b4fa78e3bdcdc
kohi-vip <lamquangkhoi2016@gmail.com>
2025-09-22 20:37:54 +0700

    update db

--- Create user and db.sql
+++ Create user and db.sql
@@ -15,3 +15,38 @@ CREATE USER 'restaurantservice'@'localhost' IDENTIFIED BY 'res';
 GRANT ALL PRIVILEGES ON restaurant_db.* TO 'restaurantservice'@'localhost';
 GRANT ALL PRIVILEGES ON *.* TO 'restaurantservice'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
+
+USE restaurant_db;
+INSERT INTO restaurants (address, image, name) VALUES
+('123 Lê Văn Sỹ, Quận 3, TP.HCM', 'images/burger_king.jpg', 'Burger King'),
+('456 Nguyễn Trãi, Quận 5, TP.HCM', 'images/kfc.jpg', 'KFC'),
+('789 Trần Hưng Đạo, Quận 1, TP.HCM', 'images/mcdonalds.jpg', 'McDonald\'s'),
+('101 Phan Xích Long, Phú Nhuận, TP.HCM', 'images/pizza_hut.jpg', 'Pizza Hut'),
+('202 Điện Biên Phủ, Bình Thạnh, TP.HCM', 'images/dominos.jpg', 'Domino\'s Pizza'),
+('303 Nguyễn Thị Minh Khai, Quận 1, TP.HCM', 'images/lotteria.jpg', 'Lotteria'),
+('404 Cách Mạng Tháng 8, Quận 10, TP.HCM', 'images/jollibee.jpg', 'Jollibee'),
+('505 Võ Văn Tần, Quận 3, TP.HCM', 'images/subway.jpg', 'Subway'),
+('606 Nguyễn Đình Chiểu, Quận 3, TP.HCM', 'images/tocotoco.jpg', 'TocoToco'),
+('707 Lý Chính Thắng, Quận 3, TP.HCM', 'images/highlands.jpg', 'Highlands Coffee');
+
+INSERT INTO menu_items (image_url, name, price, stock, restaurant_id) VALUES
+('images/burger_beef_cheese.jpg', 'Burger bò phô mai', 65000, 50, 1),
+('images/burger_spicy_chicken.jpg', 'Burger gà cay', 60000, 40, 4),
+('images/burger_veggie.jpg', 'Burger chay', 55000, 30, 1),
+('images/french_fries.jpg', 'Khoai tây chiên', 30000, 100, 1),
+('images/fried_chicken_1.jpg', 'Gà rán phần 1 miếng', 35000, 80, 2),
+('images/fried_chicken_2.jpg', 'Gà rán phần 2 miếng', 65000, 60, 5),
+('images/hotdog_classic.jpg', 'Hotdog truyền thống', 40000, 70, 1),
+('images/hotdog_cheese.jpg', 'Hotdog phô mai', 45000, 60, 1),
+('images/pizza_cheese.jpg', 'Pizza phô mai', 90000, 40, 6),
+('images/pizza_seafood.jpg', 'Pizza hải sản', 110000, 35, 3),
+('images/pizza_sausage.jpg', 'Pizza xúc xích', 95000, 45, 3),
+('images/chicken_nuggets.jpg', 'Nuggets gà', 40000, 90, 2),
+('images/sandwich_ham.jpg', 'Sandwich thịt nguội', 45000, 50, 1),
+('images/sandwich_egg.jpg', 'Sandwich trứng', 40000, 50, 1),
+('images/taco_beef.jpg', 'Taco bò', 50000, 40, 1),
+('images/taco_chicken.jpg', 'Taco gà', 50000, 40, 1),
+('images/salad_grilled_chicken.jpg', 'Salad gà nướng', 55000, 30, 2),
+('images/salad_tuna.jpg', 'Salad cá ngừ', 60000, 30, 2),
+('images/milkshake_chocolate.jpg', 'Sữa lắc socola', 45000, 60, 7),
+('images/milkshake_strawberry.jpg', 'Sữa lắc dâu', 45000, 60, 3);
