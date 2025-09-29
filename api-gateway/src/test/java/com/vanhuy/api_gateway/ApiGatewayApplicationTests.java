3c805148cbe9bc12a3c009ee0a25584a01638133
kohi-vip <lamquangkhoi2016@gmail.com>
2025-09-29 21:21:52 +0700

    Viết 1 unit test cơ bản cho API-GATEWAY

--- api-gateway/src/test/java/com/vanhuy/api_gateway/ApiGatewayApplicationTests.java
+++ api-gateway/src/test/java/com/vanhuy/api_gateway/ApiGatewayApplicationTests.java
@@ -1,13 +1,35 @@
 package com.vanhuy.api_gateway;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
 class ApiGatewayApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private WebTestClient webTestClient;
 
+    @Test
+    void contextLoads() {
+        // chỉ cần khởi động context thành công là pass
+    }
+
+    @Test
+    void helloEndpointShouldReturnMessage() {
+        webTestClient.get().uri("/hello")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(String.class).isEqualTo("Hello from API Gateway");
+    }
+
+    @Test
+    void unknownPathShouldReturn404() {
+        webTestClient.get().uri("/not-found")
+                .exchange()
+                .expectStatus().isNotFound();
+    }
 }
