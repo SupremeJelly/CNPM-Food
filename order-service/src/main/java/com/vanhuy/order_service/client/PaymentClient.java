032e1cb9220d9f291ed150af0b98fbbc53c13c2c
kohi-vip <lamquangkhoi2016@gmail.com>
2025-10-02 23:14:57 +0700

    Update chạy run mvn payment thành công

--- order-service/src/main/java/com/vanhuy/order_service/client/PaymentClient.java
+++ order-service/src/main/java/com/vanhuy/order_service/client/PaymentClient.java
@@ -1,11 +1,11 @@
 package com.yourcompany.order.client;
 
-import com.yourcompany.order.dto.PaymentRequest;
-import com.yourcompany.order.dto.PaymentResponse;
+import com.vanhuy.order_service.dto.PaymentRequest;
+import com.vanhuy.order_service.dto.PaymentResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
-@FeignClient(name = "payment-service", url = "http://localhost:8085")
+@FeignClient(name = "payment-service", url = "http://localhost:8084")
 public interface PaymentClient {
 
     @PostMapping("/api/payments/charge")
