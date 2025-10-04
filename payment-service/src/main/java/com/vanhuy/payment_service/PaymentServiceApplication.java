c2111ca83a65e9881680a3995830265883493df0
SupremeJelly <vibilinhthach@gmail.com>
2025-10-04 23:19:56 +0700

    Payment aplication n fe  update

--- payment-service/src/main/java/com/vanhuy/payment_service/PaymentServiceApplication.java
+++ payment-service/src/main/java/com/vanhuy/payment_service/PaymentServiceApplication.java
@@ -2,8 +2,10 @@ package com.vanhuy.payment_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients(basePackages = "com.vanhuy.payment_service.client")
 public class PaymentServiceApplication {
 
 	public static void main(String[] args) {
