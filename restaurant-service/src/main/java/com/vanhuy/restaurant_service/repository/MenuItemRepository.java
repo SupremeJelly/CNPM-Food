bb5c7aaab9d223c4e28b0fd131f7028020f122e6
kohi-vip <lamquangkhoi2016@gmail.com>
2025-10-05 23:28:03 +0700

    Fix stock của payment

--- restaurant-service/src/main/java/com/vanhuy/restaurant_service/repository/MenuItemRepository.java
+++ restaurant-service/src/main/java/com/vanhuy/restaurant_service/repository/MenuItemRepository.java
@@ -10,4 +10,12 @@ import java.util.List;
 
 public interface MenuItemRepository extends JpaRepository<MenuItem , Integer> {
     List<MenuItem> findByRestaurant(Restaurant restaurant);
+
+    @Modifying
+    @Query("UPDATE MenuItem m SET m.stock = m.stock - :qty WHERE m.itemId = :id AND m.stock >= :qty")
+    int decrementStockIfAvailable(@Param("id") Integer id, @Param("qty") Integer qty);
+
+    @Modifying
+    @Query("UPDATE MenuItem m SET m.stock = m.stock + :qty WHERE m.itemId = :id")
+    int incrementStock(@Param("id") Integer id, @Param("qty") Integer qty);
 }
