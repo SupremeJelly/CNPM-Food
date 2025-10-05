fda75f79cce80381e98a097b8d4024e1284f7e94
kohi-vip <lamquangkhoi2016@gmail.com>
2025-10-05 20:48:02 +0700

    Sửa status 2 bảng db

--- payment-service/src/main/java/com/vanhuy/payment_service/service/PaymentService.java
+++ payment-service/src/main/java/com/vanhuy/payment_service/service/PaymentService.java
@@ -22,7 +22,7 @@ public class PaymentService {
     private OrderClient orderClient;
 
     public Payment createPayment(Payment p) {
-        p.setStatus(PaymentStatus.PENDING);
+        p.setStatus(PaymentStatus.PAID);
         p.setCreatedAt(LocalDateTime.now());
         Payment saved = repo.save(p);
 
@@ -53,95 +53,19 @@ public class PaymentService {
         return updated;
     }
     public Payment processPayment(Integer orderId, String method) {
-    Payment payment = new Payment();
-    payment.setOrderId(orderId);
-    Payment.PaymentMethod paymentMethod = Payment.PaymentMethod.valueOf(method.toUpperCase());
-    payment.setMethod(paymentMethod);
-    payment.setStatus(PaymentStatus.SUCCESS); // giả sử thanh toán thành công
-    payment.setCreatedAt(LocalDateTime.now());
+        Payment payment = new Payment();
+        payment.setOrderId(orderId);
+        Payment.PaymentMethod paymentMethod = Payment.PaymentMethod.valueOf(method.toUpperCase());
+        payment.setMethod(paymentMethod);
+        payment.setStatus(PaymentStatus.PAID); // giả sử thanh toán thành công
+        payment.setCreatedAt(LocalDateTime.now());
 
-    Payment saved = repo.save(payment);
+        Payment saved = repo.save(payment);
 
-    // cập nhật trạng thái sang Order service
-    orderClient.updatePaymentStatus(orderId, PaymentStatus.SUCCESS.name());
+        // cập nhật trạng thái sang Order service
+        orderClient.updatePaymentStatus(orderId, PaymentStatus.PAID.name());
 
-    return saved;
+        return saved;
+    }
+
 }
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// package com.vanhuy.payment_service.service;
-
-
-// import java.time.LocalDateTime;
-// import java.util.List;
-// import java.util.Optional;
-
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.stereotype.Service;
-
-// import com.vanhuy.payment_service.model.Payment;
-// import com.vanhuy.payment_service.model.Payment.PaymentStatus;
-// import com.vanhuy.payment_service.repository.PaymentRepository;
-
-
-// @Service
-// public class PaymentService {
-
-
-//     @Autowired
-//     private PaymentRepository repo;
-
-
-//     public Payment createPayment(Payment p) {
-//         p.setStatus(PaymentStatus.PENDING);
-//         p.setCreatedAt(LocalDateTime.now());
-//         return repo.save(p);
-//     }
-
-
-//     public Optional<Payment> getById(Long id) {
-//         return repo.findById(id);
-//     }
-
-
-//     public List<Payment> getAll() {
-//         return repo.findAll();
-//     }
-
-
-//     public List<Payment> getByOrderId(Long orderId) {
-//         return repo.findByOrderId(orderId);
-//     }
-
-
-//     public Payment updateStatus(Long id, PaymentStatus status) {
-//         Payment p = repo.findById(id).orElseThrow();
-//         p.setStatus(status);
-//         orderClient.updatePaymentStatus(p.getOrderId(), status.name());
-//         return repo.save(p);
-//     }
-
-// }
