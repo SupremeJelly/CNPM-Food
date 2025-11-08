e112032322e83f76e0dc924202792d6957270720
kohi-vip <lamquangkhoi2016@gmail.com>
2025-11-08 17:03:06 +0700

    update

--- api-gateway/src/main/java/com/vanhuy/api_gateway/config/RestTemplateConfig.java
+++ api-gateway/src/main/java/com/vanhuy/api_gateway/config/RestTemplateConfig.java
@@ -1,5 +1,4 @@
 package com.vanhuy.api_gateway.config;
-
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
