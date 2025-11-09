31b8a839ad3c61e9ac69651c5d08d009ef254f3c
SupremeJelly <vibilinhthach@gmail.com>
2025-11-09 17:59:48 +0700

    fix: update API Gateway port to 32085 in frontend

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    baseUrl: 'http://localhost:9000/api/v1' // API Gateway accessible from browser
+    baseUrl: 'http://localhost:32085/api/v1' // API Gateway accessible from browser via NodePort
   };
