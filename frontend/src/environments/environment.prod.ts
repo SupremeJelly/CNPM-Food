b782931448b3a68682da9c19e0f48c525c40e465
kohi-vip <lamquangkhoi2016@gmail.com>
2025-12-01 23:50:15 +0700

    test quy trình

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,4 @@
 export const environment = {
     production: true,
-    // baseUrl: 'http://localhost:9000/api/v1' // API Gateway LoadBalancer port
-    // baseUrl: 'https://cnpm-food-api-gateway.onrender.com/api/v1'
-    baseUrl: '/api/v1'
+    baseUrl: 'https://cnpm-api-gateway.onrender.com/api/v1'
   };
