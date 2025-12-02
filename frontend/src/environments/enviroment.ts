72f45f0d772ab52b28d7bba7c8173a394603b83e
kohi-vip <lamquangkhoi2016@gmail.com>
2025-12-02 07:30:06 +0700

    làm lỗi test order

--- frontend/src/environments/enviroment.ts
+++ frontend/src/environments/enviroment.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: false,
-    baseUrl: 'http://localhost:9000/api/v1',
+    // baseUrl: 'http://localhost:9000/api/v1',
     // production: true,
     // baseUrl: 'https://cnpm-food-api-gateway-lvu6.onrender.com/api/v1',
+    baseUrl: 'https://cnpm-api-gateway.onrender.com/api/v1'
   };
--- order-service/src/test/java/com/vanhuy/order_service/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/vanhuy/order_service/OrderServiceApplicationTests.java
@@ -5,14 +5,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@TestPropertySource(properties = {
-    "spring.datasource.url=jdbc:h2:mem:testdb",
-    "spring.datasource.driver-class-name=org.h2.Driver",
-    "spring.jpa.hibernate.ddl-auto=create-drop",
-    "restaurant.service.url=http://localhost:8082",
-    "payment.service.url=http://localhost:8085",
-    "user.service.url=http://localhost:8081"
-})
+// @TestPropertySource(properties = {
+//     "spring.datasource.url=jdbc:h2:mem:testdb",
+//     "spring.datasource.driver-class-name=org.h2.Driver",
+//     "spring.jpa.hibernate.ddl-auto=create-drop",
+//     "restaurant.service.url=http://localhost:8082",
+//     "payment.service.url=http://localhost:8085",
+//     "user.service.url=http://localhost:8081"
+// })
 class OrderServiceApplicationTests {
 
 	@Test
